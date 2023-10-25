94740b6700dabc35c644bf789e1efc000d308fdf
Oluwatobi Ojelabi <tobi.oje@icloud.com>
2023-10-25 21:45:15 +0100

    Fonts and styling edits done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
 import "./styles/globals.scss";
 import Header from "./components/Header/Header";
+
+const poppins = Poppins({
+  weight: ["300", "400", "500", "600", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Rick and Morty",
@@ -14,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className={poppins.className}>
         <Header />
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import StartLoader from "./components/StartLoader/StartLoader";
 import styles from "./page.module.css";
-import { useRouter } from "next/navigation";
 import CharactersList from "./characters/CharacterList/CharacterList";
 
 export default function Home() {
